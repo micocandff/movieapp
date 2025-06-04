163c69204895095e5f8d153f1ec44308e0747dc0
micocandff <micocandrapebrian@gmail.com>
2025-06-04 11:14:31 +0700

    Fix layout bug in movie index view

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -22,9 +22,7 @@
                 </div>
             @endforeach
         </div>
-        </div>
-            <a href="{{ url('movies/create') }}" class=" btn btn-primary btn-floating float-right">+</a>
-        </div>  
+        <a href="{{ url('movies/create') }}" class="btn btn-primary btn-floating float-right">+</a>
     </div>
 @endsection
 
